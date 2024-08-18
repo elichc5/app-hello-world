9556660ef0280b6f95c788d7232609eba3fda2d4
elichc5 <jfran9555@gmail.com>
2024-08-18 07:54:33 -0500

    Corrección del mensaje

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Francisco bienbenido a tu App'
+    return 'Francisco bienvenido a tu App hecha con Python'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
